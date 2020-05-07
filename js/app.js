651e941f9047fb49a21bc06c01ceeb90ad1378a2
Jeff Balagosa <jeff.balagosa@gmail.com>
2020-05-07 10:17:01 -0700

    added a few more randomizers and added a faction/follower compatibility checker

--- js/app.js
+++ js/app.js
@@ -1,14 +1,54 @@
+const sex = ['Male', 'Female'];
 const majorFactions = [
   'The Minutemen',
   'The Institute',
   'The Railroad',
   'The Brotherhood of Steel',
 ];
+const companions = [
+  'Cait',
+  'Codsworth',
+  'Curie',
+  'Deacon',
+  'Dogmeat',
+  'John Hancock',
+  'Nick Valentine',
+  'Paladin Danse',
+  'Piper',
+  'Preston Garvey',
+  'Robert Joseph MacCready',
+  'Strong',
+  'X6-88',
+];
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+const combatStyles = ['Stealthy Assassin', 'Guns Blazing!'];
+
+let loyalty = listPicker(majorFactions);
+const mainSidekick = listPicker(companions);
+const combatStyle = listPicker(combatStyles);
+
+function listPicker(array) {
+  const listItem = Math.floor(Math.random() * Math.floor(array.length));
+  return array[listItem];
 }
 
-const randomMajorFaction = getRandomInt(majorFactions.length);
+console.log(`Protagonist: ${listPicker(sex)}`);
 
-console.log(`Loyalty: ${majorFactions[randomMajorFaction]}`);
+// check if loyalty is compatible with mainsideKick
+for (let i = 0; i < majorFactions.length; i++) {
+  if (
+    (loyalty === 'The Institute' && mainSidekick === 'Deacon') ||
+    mainSidekick === 'Paladin Danse'
+  ) {
+    loyalty = listPicker(majorFactions);
+  } else if (
+    (loyalty === 'The Railroad' && mainSidekick === 'X6-88') ||
+    mainSidekick === 'Paladin Danse'
+  ) {
+    loyalty = listPicker(majorFactions);
+  }
+}
+
+console.log(`Loyalty: ${loyalty}`);
+console.log(`Main Sidekick: ${mainSidekick}`);
+console.log(`Combat Style: ${combatStyle}`);
--- readme.txt
+++ readme.txt
@@ -1,3 +1,7 @@
 Goal:  Generate a starting plot for Fallout 4.
 
 First sub goal:  Randomize what faction the player will ultimately be loyal to.
+Done.
+Second sub goal: Randomize player's sidekick.
+Done.
+3rd sub goal:  Validate sidekick.  Some sidekicks are incompatible with faction loyalty.
