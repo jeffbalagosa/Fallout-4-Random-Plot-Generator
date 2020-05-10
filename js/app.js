9ec8a8146a9e97909207a200f6c210191d532d99
Jeff Balagosa <jeff.balagosa@gmail.com>
2020-05-09 21:50:17 -0700

    got special attribues loaded on page

--- js/app.js
+++ js/app.js
@@ -134,16 +134,35 @@ for (let i = 0; i < sex.length; i += 1) {
 }
 
 $('.character-attributes').html(
-  `<dt>Protagonist: </dt>
-  <dd>${protagonist}</dd>
-  <dt>Loyalty: </dt>
-  <dd>${loyalty}</dd>
-  <dt>Base of Opperations: </dt>
-  <dd>${mainSettlement}</dd>
-  <dt>Main Sidekick: </dt>
-  <dd>${mainSidekick}</dd>
-  <dt>Preferred Weapon Type: </dt>
-  <dd>${preferredWeapon}</dd>
-  <dt>Combat Style: </dt>
-  <dd>${combatStyle}</dd>`
+  `<div>
+    <dl>
+      <dt>Protagonist: </dt>
+      <dd>${protagonist}</dd>
+      <dt>Loyalty: </dt>
+      <dd>${loyalty}</dd>
+      <dt>Base of Opperations: </dt>
+      <dd>${mainSettlement}</dd>
+      <dt>Main Sidekick: </dt>
+      <dd>${mainSidekick}</dd>
+      <dt>Preferred Weapon Type: </dt>
+      <dd>${preferredWeapon}</dd>
+      <dt>Combat Style: </dt>
+      <dd>${combatStyle}</dd>
+    </dl>
+  </div>`
+);
+
+$('.character-attributes').append(
+  `<div>
+    <dl>
+      <dt>S.P.E.C.I.A.L.: </dt>
+      <dd>Strength = ${special[0]}</dd>
+      <dd>Perception = ${special[1]}</dd>
+      <dd>Endurance = ${special[2]}</dd>
+      <dd>Charisma = ${special[3]}</dd>
+      <dd>Intelligence = ${special[4]}</dd>
+      <dd>Agility = ${special[5]}</dd>
+      <dd>Luck = ${special[6]}</dd>
+    </dl>
+  </div>`
 );
