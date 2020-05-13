4ec6d2b84c357aa843912720529251bc1550f6f0
Jeff Balagosa <jeff.balagosa@gmail.com>
2020-05-13 16:02:50 -0700

    Split up js into data and functioning code

---
+++ js/data.js
@@ -0,0 +1,83 @@
+const sex = ['Male', 'Female'];
+const majorFactions = [
+  'The Minutemen',
+  'The Institute',
+  'The Railroad',
+  'The Brotherhood of Steel',
+];
+const companions = [
+  'Cait',
+  'Codsworth',
+  'Curie',
+  'Deacon',
+  'Dogmeat',
+  'John Hancock',
+  'Nick Valentine',
+  'Paladin Danse',
+  'Piper',
+  'Preston Garvey',
+  'Robert Joseph MacCready',
+  'Strong',
+  'X6-88',
+  'Ada (Automatron DLC)',
+  'Automatron (Automatron DLC)',
+  'Old Longfellow (Far Harbour DLC)',
+  'Fizztop Grille (Nuka-World DLC)',
+];
+const preferredWeapons = [
+  'Fisticuffs',
+  'Mele',
+  'Automatic Rifles',
+  'Automatic Pistols',
+  'Non-Automatic Pistols',
+  'Non-Automatic Rifles',
+  'Explosives',
+  'Heavy Guns',
+];
+const settlements = [
+  'Sanctuary Hills',
+  'Red Rocket Truck Stop',
+  'Abernathy Farm',
+  'Sunshine Tidings Co-op',
+  'Starlight Drive In',
+  'Tenpines Bluff',
+  'Graygarden',
+  'Oberland Station',
+  'Hangman’s Alley',
+  'Egret Tours Marina',
+  'Sommerville Place',
+  'Outpost Zimonja',
+  'Covenant',
+  'Taffington Boathouse',
+  'Greentop Nursery',
+  'The Slog',
+  'Finch Farm',
+  'Country Crossing',
+  'Coastal Cottage',
+  'Kingsport Lighthouse',
+  'Croup Manor',
+  'Nordhagen Beach',
+  'Jamiaca Plain',
+  'Murkwater Construction Site',
+  'Warwick Homestead',
+  'Spectacle Island',
+  'The Castle',
+  'Bunker Hill',
+  'Boston Airport',
+  'The Mechanist’s Lair (Automatron DLC)',
+  'Longfellow’s Cabin (Far Harbour DLC)',
+  'National Park Visitor’s Centre (Far Harbour DLC)',
+  'Dalton Farm (Far Harbour DLC)',
+  'Echo Lake Lumber (Far Harbour DLC)',
+  'Vault 88 (Vault-Tec Workshop DLC)',
+  'Nuka-World Red Rocket (Nuka-World DLC)',
+];
+const combatStyles = ['Stealthy Assassin', 'Boisterous Murderer!'];
+
+let loyalty;
+let mainSidekick;
+let combatStyle;
+let protagonist;
+let preferredWeapon;
+let mainSettlement;
+let special = [];
